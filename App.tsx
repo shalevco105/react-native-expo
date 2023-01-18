407756ef5d0e2981c4ccbdd63f85f2882bbfd911
Rom Bukobza <romrom20022002@gmail.com>
2023-01-18 19:39:39 +0200

    add react native elements

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, Pressable, StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Button } from '@rneui/base';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>We have to be millioners already</Text>
-      <Pressable style={styles.button} onPress={() => alert('Hello World')}>
+      <Pressable style={styles.button} onPress={() => alert('Money on the floor')}>
         <Text style={{ color: 'white' }}>Press me</Text>
       </Pressable>
+      <Text><Button title="בראון גיי"/></Text>
       <StatusBar style="auto" />
     </View>
   );
